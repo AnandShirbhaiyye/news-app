29f2405fe86264d38345b3664feca83580c7b68a
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-10-04 14:27:00 +0530

    news app is ready

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// d17b81c0712d431688c1cdfe245707bc
-
 // bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
